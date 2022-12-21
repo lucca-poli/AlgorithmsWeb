7d1b8141b3d379c82161241957e20a476d0c1500
lucca-poli <lucca10azevedo@usp.br>
2022-12-21 01:25:44 -0300

    working kinda properly

--- Pages/Home/Home.js
+++ Pages/Home/Home.js
@@ -7,12 +7,28 @@ const dataStructuresButton = document.querySelector(
 const algorithmsButton = document.querySelector(`.algorithms-menu-button`);
 
 dataStructuresButton.addEventListener("click", () => {
-  //the listener is working but not the .style inside the listener
-  if (window.getComputedStyle(dataStructuresButton).display === "none") {
+  //the transition is not smooth
+  if (window.getComputedStyle(dataStructures).display === "none") {
     dataStructures.style.display = "block";
     dataStructures.style.transform = "translateY(0%)";
+    dataStructures.style.opacity = 1;
+    dataStructures.style.transition =
+      "transform 2s ease-out, opacity 2s ease-out";
   } else {
     dataStructures.style.display = "none";
     dataStructures.style.transform = "translateY(-100%)";
+    dataStructures.style.opacity = 0;
+  }
+});
+
+algorithmsButton.addEventListener("click", () => {
+  if (window.getComputedStyle(algorithms).display === "none") {
+    algorithms.style.display = "block";
+    algorithms.style.transform = "translateY(0%)";
+    algorithms.style.opacity = 1;
+  } else {
+    algorithms.style.display = "none";
+    algorithms.style.transform = "translateY(-100%)";
+    algorithms.style.opacity = 0;
   }
 });
