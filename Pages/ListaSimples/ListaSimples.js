346867c1b26460e3d2754c7ef1836954dea60db7
lucca-poli <lucca10azevedo@usp.br>
2022-12-22 21:14:55 -0300

    Update ListaSimples.js

--- Pages/ListaSimples/ListaSimples.js
+++ Pages/ListaSimples/ListaSimples.js
@@ -1,7 +1,72 @@
+class Renderer {
+  constructor(value, pointer) {
+    this.elementDesign = this.designElement(value, pointer);
+    this.listDesign = this.designList();
+  }
+
+  designList() {
+    const list = document.createElement(`div`);
+    const firstElement = new Renderer("L", "pointer");
+    list.appendChild(firstElement.elementDesign);
+
+    list.style.display = "flex";
+    list.style.flexDirection = "row";
+    list.style.width = "100vw";
+
+    return list;
+  }
+
+  designElement(left, right) {
+    const element = document.createElement(`div`);
+    const elementValueLeft = document.createElement(`div`);
+    const elementValueRight = document.createElement(`div`);
+    const pointerImage = document.createElement(`img`);
+    pointerImage.src = "../../Assets/longRightArrow.svg";
+
+    pointerImage.style.position = `relative`;
+    pointerImage.style.left = `2.5em`;
+    pointerImage.style.width = "3vw";
+    pointerImage.style.transform = `scale(3,1)`;
+
+    element.style.display = "flex";
+    element.style.flexDirection = "row";
+    element.style.height = "3vh";
+    element.style.width = "6vw";
+
+    elementValueLeft.style.width = "3vw";
+    elementValueLeft.style.height = "3vh";
+    elementValueLeft.style.display = "flex";
+    elementValueLeft.style.alignItems = "center";
+    elementValueLeft.style.justifyContent = "center";
+    elementValueLeft.style.border = "2px solid black";
+
+    elementValueRight.style.width = "3vw";
+    elementValueRight.style.height = "3vh";
+    elementValueRight.style.display = "flex";
+    elementValueRight.style.alignItems = "center";
+    elementValueRight.style.justifyContent = "center";
+    elementValueRight.style.border = "2px solid black";
+
+    elementValueLeft.innerHTML = left;
+    if (right === "null") {
+      elementValueRight.innerHTML = "/";
+    }
+    if (right === "pointer") {
+      elementValueRight.appendChild(pointerImage);
+    }
+    element.appendChild(elementValueLeft);
+    element.appendChild(elementValueRight);
+    return element;
+  }
+}
+
 class ELement {
   constructor(elementValue) {
     this.elementValue = elementValue;
     this.nextElement = null;
+
+    const elementDesign = new Renderer(value, "null");
+    this.elementHTML = elementDesign.elementDesign;
   }
 }
 
@@ -9,19 +74,8 @@ class ListaLigada {
   constructor() {
     this.start = null;
 
-    const startBlock = document.createElement(`div`);
-    const seta = document.createElement(`i`);
-    seta.classList.add("fa-solid", "fa-arrow-right-long");
-    const display = document.querySelector(`#display`);
-    startBlock.innerHTML = '<i class="fa-solid fa-arrow-right-long"></i>';
-    startBlock.style.width = "1.5em";
-    startBlock.style.height = "1.5em";
-    startBlock.style.padding = "0.2em";
-    startBlock.style.display = "flex";
-    startBlock.style.alignItems = "center";
-    startBlock.style.justifyContent = "center";
-    startBlock.style.border = "2px solid black";
-    display.appendChild(startBlock);
+    const startDesign = new Renderer("L", "pointer");
+    this.startHTML = startDesign.listDesign;
   }
 
   listInsert(element) {
@@ -31,7 +85,12 @@ class ListaLigada {
     }
     element.nextElement = this.start;
     this.start = element;
+    element.elementHTML.style.marginLeft = "2em";
   }
 }
 
 const listaPrincipal = new ListaLigada();
+const elementTest = new Element(2);
+
+const mainDisplay = document.querySelector(`#display`);
+mainDisplay.appendChild(listaPrincipal.startHTML);
