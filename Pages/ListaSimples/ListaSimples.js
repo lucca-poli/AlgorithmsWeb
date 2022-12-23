64557178f091f5f012f548ab649b0d5948c92ad0
lucca-poli <lucca10azevedo@usp.br>
2022-12-23 15:19:41 -0300

    changed to tyoescript

--- Pages/ListaSimples/ListaSimples.js
+++ Pages/ListaSimples/ListaSimples.js
@@ -1,96 +1,92 @@
-class Renderer {
-  constructor(value, pointer) {
-    this.elementDesign = this.designElement(value, pointer);
-    this.listDesign = this.designList();
-  }
-
-  designList() {
-    const list = document.createElement(`div`);
-    const firstElement = new Renderer("L", "pointer");
-    list.appendChild(firstElement.elementDesign);
-
-    list.style.display = "flex";
-    list.style.flexDirection = "row";
-    list.style.width = "100vw";
-
-    return list;
-  }
-
-  designElement(left, right) {
-    const element = document.createElement(`div`);
-    const elementValueLeft = document.createElement(`div`);
-    const elementValueRight = document.createElement(`div`);
-    const pointerImage = document.createElement(`img`);
-    pointerImage.src = "../../Assets/longRightArrow.svg";
-
-    pointerImage.style.position = `relative`;
-    pointerImage.style.left = `2.5em`;
-    pointerImage.style.width = "3vw";
-    pointerImage.style.transform = `scale(3,1)`;
-
-    element.style.display = "flex";
-    element.style.flexDirection = "row";
-    element.style.height = "3vh";
-    element.style.width = "6vw";
-
-    elementValueLeft.style.width = "3vw";
-    elementValueLeft.style.height = "3vh";
-    elementValueLeft.style.display = "flex";
-    elementValueLeft.style.alignItems = "center";
-    elementValueLeft.style.justifyContent = "center";
-    elementValueLeft.style.border = "2px solid black";
-
-    elementValueRight.style.width = "3vw";
-    elementValueRight.style.height = "3vh";
-    elementValueRight.style.display = "flex";
-    elementValueRight.style.alignItems = "center";
-    elementValueRight.style.justifyContent = "center";
-    elementValueRight.style.border = "2px solid black";
-
-    elementValueLeft.innerHTML = left;
-    if (right === "null") {
-      elementValueRight.innerHTML = "/";
+var Renderer = /** @class */ (function () {
+    function Renderer() {
     }
-    if (right === "pointer") {
-      elementValueRight.appendChild(pointerImage);
+    Renderer.prototype.designList = function () {
+        var list = document.createElement("div");
+        list.appendChild(this.designElement("L", "pointer"));
+        list.style.display = "flex";
+        list.style.justifyContent = "center";
+        list.style.flexDirection = "row";
+        list.style.width = "100vw";
+        list.style.padding = "1em";
+        return list;
+    };
+    Renderer.prototype.designElement = function (left, right) {
+        var element = document.createElement("div");
+        var elementValueLeft = document.createElement("div");
+        var elementValueRight = document.createElement("div");
+        var pointerImage = document.createElement("img");
+        pointerImage.src = "../../Assets/longRightArrow.svg";
+        pointerImage.style.position = "relative";
+        pointerImage.style.left = "25px";
+        pointerImage.style.width = "30px";
+        pointerImage.style.transform = "scale(3,1)";
+        element.style.display = "flex";
+        element.style.flexDirection = "row";
+        element.style.height = "30px";
+        element.style.width = "60px";
+        element.style.marginLeft = "40px";
+        elementValueLeft.style.width = "30px";
+        elementValueLeft.style.height = "30px";
+        elementValueLeft.style.display = "flex";
+        elementValueLeft.style.alignItems = "center";
+        elementValueLeft.style.justifyContent = "center";
+        elementValueLeft.style.border = "2px solid black";
+        elementValueRight.style.width = "30px";
+        elementValueRight.style.height = "30px";
+        elementValueRight.style.display = "flex";
+        elementValueRight.style.alignItems = "center";
+        elementValueRight.style.justifyContent = "center";
+        elementValueRight.style.border = "2px solid black";
+        elementValueLeft.innerHTML = left.toString();
+        if (right === "null") {
+            elementValueRight.innerHTML = "/";
+        }
+        if (right === "pointer") {
+            elementValueRight.appendChild(pointerImage);
+        }
+        element.appendChild(elementValueLeft);
+        element.appendChild(elementValueRight);
+        return element;
+    };
+    return Renderer;
+}());
+var Elemento = /** @class */ (function () {
+    function Elemento(elementValue) {
+        this.elementValue = elementValue;
+        this.nextElement = null;
+        var elementDesign = new Renderer();
+        this.elementHTML = elementDesign.designElement(elementValue, "null");
     }
-    element.appendChild(elementValueLeft);
-    element.appendChild(elementValueRight);
-    return element;
-  }
-}
-
-class ELement {
-  constructor(elementValue) {
-    this.elementValue = elementValue;
-    this.nextElement = null;
-
-    const elementDesign = new Renderer(value, "null");
-    this.elementHTML = elementDesign.elementDesign;
-  }
-}
-
-class ListaLigada {
-  constructor() {
-    this.start = null;
-
-    const startDesign = new Renderer("L", "pointer");
-    this.startHTML = startDesign.listDesign;
-  }
-
-  listInsert(element) {
-    if (this.start === null) {
-      this.start = element;
-      return;
+    return Elemento;
+}());
+var ListaLigada = /** @class */ (function () {
+    function ListaLigada() {
+        this.start = null;
+        var startDesign = new Renderer();
+        this.startHTML = startDesign.designList();
     }
-    element.nextElement = this.start;
-    this.start = element;
-    element.elementHTML.style.marginLeft = "2em";
-  }
-}
-
-const listaPrincipal = new ListaLigada();
-const elementTest = new Element(2);
-
-const mainDisplay = document.querySelector(`#display`);
-mainDisplay.appendChild(listaPrincipal.startHTML);
+    ListaLigada.prototype.listInsert = function (element) {
+        if (this.start === null) {
+            this.start = element;
+            this.startHTML.appendChild(element.elementHTML);
+            return;
+        }
+        element.nextElement = this.start;
+        this.start = element;
+        var startDesign = new Renderer();
+        element.elementHTML = startDesign.designElement(element.elementValue, "pointer");
+        this.startHTML.insertBefore(element.elementHTML, this.startHTML.childNodes[1]);
+    };
+    return ListaLigada;
+}());
+var listaPrincipal = new ListaLigada();
+var firstElement = new Elemento(2);
+var secondElement = new Elemento(5);
+var thirdElement = new Elemento(11);
+listaPrincipal.listInsert(firstElement);
+listaPrincipal.listInsert(secondElement);
+listaPrincipal.listInsert(thirdElement);
+var test = listaPrincipal.startHTML;
+var mainDisplay = document.querySelector("#display");
+mainDisplay.appendChild(test);
---
+++ Pages/ListaSimples/ListaSimples.ts
@@ -0,0 +1,120 @@
+class Renderer {
+  designList() {
+    const list = document.createElement(`div`);
+
+    list.appendChild(this.designElement("L", "pointer"));
+
+    list.style.display = "flex";
+    list.style.justifyContent = "center";
+    list.style.flexDirection = "row";
+    list.style.width = "100vw";
+    list.style.padding = "1em";
+
+    return list;
+  }
+
+  designElement(left: number | string, right: string) {
+    const element = document.createElement(`div`);
+    const elementValueLeft = document.createElement(`div`);
+    const elementValueRight = document.createElement(`div`);
+    const pointerImage = document.createElement(`img`);
+    pointerImage.src = "../../Assets/longRightArrow.svg";
+
+    pointerImage.style.position = `relative`;
+    pointerImage.style.left = `25px`;
+    pointerImage.style.width = "30px";
+    pointerImage.style.transform = `scale(3,1)`;
+
+    element.style.display = "flex";
+    element.style.flexDirection = "row";
+    element.style.height = "30px";
+    element.style.width = "60px";
+    element.style.marginLeft = "40px";
+
+    elementValueLeft.style.width = "30px";
+    elementValueLeft.style.height = "30px";
+    elementValueLeft.style.display = "flex";
+    elementValueLeft.style.alignItems = "center";
+    elementValueLeft.style.justifyContent = "center";
+    elementValueLeft.style.border = "2px solid black";
+
+    elementValueRight.style.width = "30px";
+    elementValueRight.style.height = "30px";
+    elementValueRight.style.display = "flex";
+    elementValueRight.style.alignItems = "center";
+    elementValueRight.style.justifyContent = "center";
+    elementValueRight.style.border = "2px solid black";
+
+    elementValueLeft.innerHTML = left.toString();
+    if (right === "null") {
+      elementValueRight.innerHTML = "/";
+    }
+    if (right === "pointer") {
+      elementValueRight.appendChild(pointerImage);
+    }
+    element.appendChild(elementValueLeft);
+    element.appendChild(elementValueRight);
+    return element;
+  }
+}
+
+class Elemento {
+  elementValue: number;
+  nextElement: Elemento | null;
+  elementHTML: Node;
+
+  constructor(elementValue: number) {
+    this.elementValue = elementValue;
+    this.nextElement = null;
+
+    const elementDesign = new Renderer();
+    this.elementHTML = elementDesign.designElement(elementValue, "null");
+  }
+}
+
+class ListaLigada {
+  start: Elemento | null;
+  startHTML: Node;
+
+  constructor() {
+    this.start = null;
+
+    const startDesign = new Renderer();
+    this.startHTML = startDesign.designList();
+  }
+
+  listInsert(element: Elemento) {
+    if (this.start === null) {
+      this.start = element;
+      this.startHTML.appendChild(element.elementHTML);
+      return;
+    }
+    element.nextElement = this.start;
+    this.start = element;
+
+    const startDesign = new Renderer();
+    element.elementHTML = startDesign.designElement(
+      element.elementValue,
+      "pointer"
+    );
+
+    this.startHTML.insertBefore(
+      element.elementHTML,
+      this.startHTML.childNodes[1]
+    );
+  }
+}
+
+const listaPrincipal = new ListaLigada();
+const firstElement = new Elemento(2);
+const secondElement = new Elemento(5);
+const thirdElement = new Elemento(11);
+
+listaPrincipal.listInsert(firstElement);
+listaPrincipal.listInsert(secondElement);
+listaPrincipal.listInsert(thirdElement);
+
+const test = listaPrincipal.startHTML;
+
+const mainDisplay = document.querySelector("#display") as HTMLElement;
+mainDisplay.appendChild(test);
